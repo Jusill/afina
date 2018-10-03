b0eaa0bd9b0f869512c057339c01736d7ba4ac63
Jusill <Jusill98@gmail.com>
2018-10-03 22:30:48 +0300

    Add files via upload

--- src/network/mt_blocking/ServerImpl.h
+++ src/network/mt_blocking/ServerImpl.h
@@ -3,11 +3,14 @@
 
 #include <atomic>
 #include <thread>
+#include <map>
+#include <condition_variable>
 
 #include <afina/network/Server.h>
 
-namespace spdlog {
-class logger;
+namespace spdlog
+{
+  class logger;
 }
 
 namespace Afina {
@@ -18,40 +21,49 @@ namespace MTblocking {
  * # Network resource manager implementation
  * Server that is spawning a separate thread for each connection
  */
-class ServerImpl : public Server {
-public:
-    ServerImpl(std::shared_ptr<Afina::Storage> ps, std::shared_ptr<Logging::Service> pl);
-    ~ServerImpl();
+class ServerImpl : public Server
+{
+  public:
+      ServerImpl(std::shared_ptr<Afina::Storage> ps, std::shared_ptr<Logging::Service> pl);
+      ~ServerImpl();
 
-    // See Server.h
-    void Start(uint16_t port, uint32_t, uint32_t) override;
+      // See Server.h
+      void Start(uint16_t port, uint32_t, uint32_t) override;
 
-    // See Server.h
-    void Stop() override;
+      // See Server.h
+      void Stop() override;
 
-    // See Server.h
-    void Join() override;
+      // See Server.h
+      void Join() override;
 
-protected:
-    /**
-     * Method is running in the connection acceptor thread
-     */
-    void OnRun();
+  protected:
+      /**
+       * Method is running in the connection acceptor thread
+       */
+      void OnRun();
 
-private:
-    // Logger instance
-    std::shared_ptr<spdlog::logger> _logger;
+  private:
+      // Logger instance
+      std::shared_ptr<spdlog::logger> _logger;
 
-    // Atomic flag to notify threads when it is time to stop. Note that
-    // flag must be atomic in order to safely publisj changes cross thread
-    // bounds
-    std::atomic<bool> running;
+      // Atomic flag to notify threads when it is time to stop. Note that
+      // flag must be atomic in order to safely publisj changes cross thread
+      // bounds
+      std::atomic<bool> running;
 
-    // Server socket to accept connections on
-    int _server_socket;
+      // Server socket to accept connections on
+      int _server_socket;
 
-    // Thread to run network on
-    std::thread _thread;
+      // Thread to run network on
+      std::thread _thread;
+
+      uint32_t max_workers;
+
+      void WorkerFunction(int client_socket);
+
+      std::map<int, std::thread> workers;
+      std::condition_variable cv;
+      std::mutex mut;
 };
 
 } // namespace MTblocking
